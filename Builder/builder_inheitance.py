dc302a957a4aca88e322aed6902a341cd12a293e
Tim W-Fitnum <timwfitnum@gmail.com>
2023-01-21 18:26:08 +0000

    Builder

---
+++ Builder/builder_inheitance.py
@@ -0,0 +1,46 @@
+class Person:
+    def __init__(self):
+        self.name = None
+        self.position = None
+        self.date_of_birth = None
+
+    def __str__(self):
+        return (
+            f"{self.name} born on {self.date_of_birth} " + f"works as {self.position}"
+        )
+
+    @staticmethod
+    def new():
+        return PersonBuilder()
+
+
+class PersonBuilder:
+    def __init__(self) -> None:
+        self.person = Person()
+
+    def build(self):
+        return self.person
+
+
+class PersonInfoBuilder(PersonBuilder):
+    def called(self, name):
+        self.person.name = name
+        return self
+
+
+class PersonJobBuilder(PersonInfoBuilder):
+    def works_as_a(self, position):
+        self.person.position = position
+        return self
+
+
+class PersonBirthDateBuilder(PersonJobBuilder):
+    def born(self, date_of_birth):
+        self.person.date_of_birth = date_of_birth
+        return self
+
+
+pb = PersonBirthDateBuilder()
+me = pb.called("Tim").works_as_a("Developer").born("15/06/1998").build()
+
+print(me)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-design-patterns
 Going through many python design patterns to learn and solidify knowledge and understanding.
+Following along with the udemy course found here: 
+https://www.udemy.com/course/design-patterns-python/
